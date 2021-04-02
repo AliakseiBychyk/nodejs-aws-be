02eb50a7f7820ce214586626b85be028db402dd3
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-04-02 18:28:13 +0300

    handlers

---
+++ shop-info-service/handler.ts
@@ -0,0 +1,6 @@
+import 'source-map-support/register';
+
+import { getShopInfo } from './src/handlers/getShopInfo';
+import { getShopAdminInfo } from './src/handlers/getShopAdminInfo';
+
+export { getShopInfo, getShopAdminInfo };
