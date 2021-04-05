7af2006cd6b48ace063e65ef5f91e74408dd76a4
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-04-05 17:58:42 +0300

    serverless in yaml format instead of ts

--- shop-info-service/src/handlers/getShopInfo.ts
+++ shop-info-service/src/handlers/getShopInfo.ts
@@ -1,7 +1,9 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const getShopInfo: APIGatewayProxyHandler = async () => {
+export const getShopInfo: APIGatewayProxyHandler = async (event) => {
+  console.log('lambda invocation with event:', event);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
