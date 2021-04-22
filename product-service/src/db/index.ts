87c5035b341f6105ea20e1f7c1a13a77ed4c74fd
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-04-22 17:24:50 +0300

    db client

---
+++ product-service/src/db/index.ts
@@ -0,0 +1,18 @@
+import { Client } from 'pg';
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectTimeoutMillis: 5000,
+};
+
+const client = new Client(dbOptions);
+
--- product-service/src/handlers/getProductsList.ts
+++ product-service/src/handlers/getProductsList.ts
@@ -1,20 +1,16 @@
-import {APIGatewayProxyHandler} from 'aws-lambda';
+import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import products from './productsList.json';
 
-const {PG_HOST, PG_DATABASE, PG_USERNAME, PG_PASSWORD} = process.env;
-
 export const getProductsList : APIGatewayProxyHandler = async(event) => {
   console.log('getProductsList lambda invocation with event:', event);
-
-  console.log('ENV variables: ', PG_HOST, PG_PASSWORD, PG_DATABASE, PG_USERNAME);
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
+      'Access-Control-Allow-Credentials': true,
     },
     body: JSON.stringify(products)
   };
