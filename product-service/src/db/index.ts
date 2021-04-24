4f468c5ac108c45e57b7943b7eaa26b54274194a
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-04-24 18:13:23 +0300

    improve dbService logic

--- product-service/src/db/index.ts
+++ product-service/src/db/index.ts
@@ -1,28 +1,34 @@
 import { Client, ClientConfig } from 'pg';
-
+import { IProduct } from '../models/product';
 import config from './config';
 
 const { database: dbConfig }: { database: ClientConfig } = config;
 class dbService {
   private client: Client;
+  private result: IProduct[] | null;
 
   constructor() {
-    this.client = new Client(dbConfig)
+    this.result = null;
     console.log('db config: ', dbConfig)
   }
 
   public async executeQuery(query: string) {   
-    await this.client.connect()
+    this.client = new Client(dbConfig);
+
+    await this.client.connect();
     try { 
       const { rows } = await this.client.query(query);
       console.log('received rows:', rows)
-      return rows
+      this.result = rows;
     } catch (err) {
       console.error(err);
       throw err;
     } finally {
       this.client.end();
+      console.log('connection closed');
     }
+
+    return this.result;
   };
 };
 
