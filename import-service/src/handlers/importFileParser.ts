165b47d5c17b4913cf7e8c89d1996a438dbfc9b3
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-05-09 15:59:09 +0300

    putObject

--- import-service/src/handlers/importFileParser.ts
+++ import-service/src/handlers/importFileParser.ts
@@ -4,7 +4,7 @@ import S3 from 'aws-sdk/clients/s3';
 import csv from 'csv-parser';
 
 
-const Bucket = process.env.BUCKET_NAME;
+const BUCKET = process.env.BUCKET_NAME;
 
 const s3 = new S3({
   region: 'eu-west-1',
@@ -13,22 +13,28 @@ const s3 = new S3({
 
 export const importFileParser: S3Handler = async (event) => {
   console.log('importFileParser lambda invocation with event:', event);
+  console.log('existing object::', event.Records[0].s3.object)
 
   const results = [];
 
   const uploadedFilesPromises = event.Records.map(record => (
     new Promise(resolve => {
       const params: S3.Types.GetObjectRequest = {
-        Bucket,
+        Bucket: BUCKET,
         Key: record.s3.object.key,
       };
 
       const s3Stream = s3.getObject(params).createReadStream();
+      const writeJson = async (params) => {
+        return await s3.putObject(params).promise();
+      };
 
       s3Stream
         .pipe(csv())
         .on('data', (data) => {
           console.log('data from stream:', data);
+          const parsedFile = record.s3.object.key.replace('.csv', '.json');
+          writeJson({ Bucket: BUCKET, Key: `parsed/${parsedFile}`, Body: JSON.stringify(data) });
           results.push(data);
         })
         .on('error', (error) => {
@@ -37,6 +43,7 @@ export const importFileParser: S3Handler = async (event) => {
         })
         .on('end', () => {
           console.log('results:', results);
+          
           resolve(results);
         });
       
