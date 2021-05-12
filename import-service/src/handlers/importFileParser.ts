ef21eb34cbb8be9bbdffb392e44e85fbafe84208
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2021-05-12 08:41:40 +0300

    place .json into /parsed folder

--- import-service/src/handlers/importFileParser.ts
+++ import-service/src/handlers/importFileParser.ts
@@ -1,3 +1,4 @@
+import stream from 'stream';
 import { S3Handler } from 'aws-lambda';
 import 'source-map-support/register';
 import S3 from 'aws-sdk/clients/s3';
@@ -11,42 +12,47 @@ const s3 = new S3({
   signatureVersion: 'v4',
 });
 
+const createObjectToJsonLineStream = () => (
+  new stream.Transform({
+    writableObjectMode: true,
+    transform(chunk, encoding, callback) {
+      const line = `${JSON.stringify(chunk)}\n`;
+      callback(null, line);
+    }
+  })
+);
+
 export const importFileParser: S3Handler = async (event) => {
   console.log('importFileParser lambda invocation with event:', event);
   console.log('existing object::', event.Records[0].s3.object)
 
-  const results = [];
-
   const uploadedFilesPromises = event.Records.map(record => (
-    new Promise(resolve => {
+    new Promise((resolve, reject) => {
       const params: S3.Types.GetObjectRequest = {
         Bucket: BUCKET,
         Key: record.s3.object.key,
       };
 
-      const s3Stream = s3.getObject(params).createReadStream();
-      const writeJson = async (params) => {
-        return await s3.putObject(params).promise();
-      };
-
-      s3Stream
+      let pipeline = s3.getObject(params)
+        .createReadStream()
+        .on('error', err => reject(err))
         .pipe(csv())
-        .on('data', (data) => {
-          console.log('data from stream:', data);
-          const parsedFile = record.s3.object.key.replace('.csv', '.json');
-          writeJson({ Bucket: BUCKET, Key: `parsed/${parsedFile}`, Body: JSON.stringify(data) });
-          results.push(data);
-        })
-        .on('error', (error) => {
-          console.error('something went wrong:', error.message);
-          throw error;
-        })
-        .on('end', () => {
-          console.log('results:', results);
-          
-          resolve(results);
-        });
-      
+        .pipe(createObjectToJsonLineStream());
+
+      const outputParams: S3.Types.PutObjectRequest = {
+        Bucket: BUCKET,
+        Key: record.s3.object.key
+          .replace('.csv', '.json')
+          .replace('uploaded', 'parsed')
+      };
+      outputParams.Body= pipeline;
+      console.log('outputParams', outputParams);
+
+      s3.upload(outputParams)
+        .send((err, data)=> {
+          if (err) reject(err);
+          resolve(data);
+        })      
     })
   ));
 
