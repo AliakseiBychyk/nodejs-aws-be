b79d34fd2bbd494bce7bd00653616885aa35b482
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2021-05-16 23:28:49 +0300

    sqs queue

--- import-service/handler.ts
+++ import-service/handler.ts
@@ -2,5 +2,6 @@ import 'source-map-support/register';
 
 import { importProductsFile } from './src/handlers/importProductsFile';
 import { importFileParser } from './src/handlers/importFileParser';
+import { catalogBatchProcess } from './src/handlers/catalogBatchProcess';
 
-export { importProductsFile, importFileParser };
+export { importProductsFile, importFileParser, catalogBatchProcess };
