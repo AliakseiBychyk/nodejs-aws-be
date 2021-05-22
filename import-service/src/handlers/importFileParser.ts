affeeeedd77f6bd153208d3c9728341bf2ddd422
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-05-22 17:13:52 +0300

    implement moving processed objects into new folder

--- import-service/src/handlers/importFileParser.ts
+++ import-service/src/handlers/importFileParser.ts
@@ -1,4 +1,3 @@
-import stream from 'stream';
 import { S3Handler } from 'aws-lambda';
 import 'source-map-support/register';
 import S3 from 'aws-sdk/clients/s3';
@@ -12,50 +11,70 @@ const s3 = new S3({
   signatureVersion: 'v4',
 });
 
-const createObjectToJsonLineStream = () => (
-  new stream.Transform({
-    writableObjectMode: true,
-    transform(chunk, encoding, callback) {
-      const line = `${JSON.stringify(chunk)}\n`;
-      callback(null, line);
-    }
-  })
-);
-
 export const importFileParser: S3Handler = async (event) => {
   console.log('importFileParser lambda invocation with event:', event);
 
+  const moveProcessedFiles = async (event) => {
+    
+    for (const record of event.Records) {
+      console.log('key:', record.s3.object.key);
+
+      await s3.copyObject({
+        Bucket: BUCKET,
+        CopySource: BUCKET + '/' + record.s3.object.key,
+        Key: record.s3.object.key.replace('uploaded', 'processed')
+      }).promise();
+      
+      await s3.deleteObject({
+        Bucket: BUCKET,
+        Key: record.s3.object.key
+      }).promise();
+
+      console.log(`Object from ${record.s3.object.key} moved to ${record.s3.object.key.replace('uploaded', 'processed')} `)
+    }
+
+    return {
+      statusCode: 202
+    };
+  };
+
+  const results = [];
+
   const uploadedFilesPromises = event.Records.map(record => (
-    new Promise((resolve, reject) => {
+    new Promise(resolve => {
       const params: S3.Types.GetObjectRequest = {
         Bucket: BUCKET,
         Key: record.s3.object.key,
       };
 
-      const pipeline = s3.getObject(params)
-        .createReadStream()
-        .on('error', err => reject(err))
+      const s3Stream = s3.getObject(params).createReadStream();
+
+      s3Stream
         .pipe(csv())
-        .pipe(createObjectToJsonLineStream());
-
-      const outputParams: S3.Types.PutObjectRequest = {
-        Bucket: BUCKET,
-        Key: record.s3.object.key
-          .replace('.csv', '.json')
-          .replace('uploaded', 'parsed')
-      };
-      outputParams.Body= pipeline;
-      console.log('outputParams', outputParams);
-
-      s3.upload(outputParams).promise()
-        .then(data => resolve(data))
-        .catch(err => reject(err));  
+        .on('data', async (data) => {
+          console.log('data from stream:', data);
+          results.push(data);
+        })
+        .on('error', (error) => {
+          console.error('something went wrong:', error.message);
+          throw error;
+        })
+        .on('end', () => {
+          console.log('results:', results);
+          
+          resolve(results);
+        });
+      
     })
   ));
 
+
+
   try {
-    const results = await Promise.all(uploadedFilesPromises);
-    console.log('final results:', results)
+    const [ result ] = await Promise.all(uploadedFilesPromises);
+    console.log('Parsed result: ', result);
+
+    await moveProcessedFiles(event);
 
     return {
       statusCode: 202,
