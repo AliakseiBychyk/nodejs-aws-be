6927087fb1e18d4e158c229db9d73dec40029a39
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2021-05-22 15:59:26 +0300

    improve weather-service

--- weather-info-service/src/handlers/getWeatherInfo.ts
+++ weather-info-service/src/handlers/getWeatherInfo.ts
@@ -2,17 +2,21 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import axios from 'axios';
 import 'source-map-support/register';
 
+const { OPEN_WEATHER_API_KEY } = process.env;
+
 export const getWeatherInfo: APIGatewayProxyHandler = async (event) => {
   console.log('lambda invocation with event:', event);
   console.log('multiValueQueryStringParameters:', event.multiValueQueryStringParameters)
 
   const city = event.multiValueQueryStringParameters.city[0];
-  const APIKey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; // API Key has been hidden
+  const APIKey = OPEN_WEATHER_API_KEY;
 
   let weatherResponse;
   try {
     weatherResponse = await axios
       .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`) 
+    
+    console.log('weatherResponse: ', weatherResponse);
   } catch (e) {
     console.error(e);
     return {
