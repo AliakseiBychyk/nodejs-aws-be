05b15e1785b58091db7b9687b70222bedfea3af8
Aliaksei Bychyk <aliaksei_bychyk@epam.com>
2021-06-06 13:11:29 +0300

    refactoring sns

--- product-service/src/handlers/catalogBatchProcess.ts
+++ product-service/src/handlers/catalogBatchProcess.ts
@@ -1,6 +1,10 @@
 import { SQSHandler } from 'aws-lambda';
+import { SNS } from 'aws-sdk';
 import 'source-map-support/register';
 import dbService from '../db';
+
+const { SNS_TOPIC_ARN } = process.env;
+const sns = new SNS({ region: 'eu-west-1' });
 
 export const catalogBatchProcess: SQSHandler = async (event) => {
   console.log('catalogBatchProcess lambda invocation with event:', event);
@@ -8,8 +12,17 @@ export const catalogBatchProcess: SQSHandler = async (event) => {
   const createNewProducts = async (event) => {
     for (const record of event.Records) {
       const { description, price, title, image } = JSON.parse(record.body);
-      
-      await dbService.createProduct(title, description, Number(price), image);
+      try {
+        await dbService.createProduct(title, description, Number(price), image);
+        await sns.publish({
+          Subject: 'Product created',
+          Message: `Product created: \n${record.body}`,
+          TopicArn: SNS_TOPIC_ARN
+        }).promise();
+      } catch(e) {
+        console.error('Failure occured', e);
+        throw e;
+      } 
     }
     return {
       statusCode: 202
@@ -17,7 +30,6 @@ export const catalogBatchProcess: SQSHandler = async (event) => {
   }
 
   await createNewProducts(event);
-
 
   return {
     statusCode: 200
